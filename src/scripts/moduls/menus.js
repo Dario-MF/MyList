05b8571db2b5abcc54fe7be1dc2090976ce5feb7
Dario MF <ferrada_86@hotmail.com>
2020-07-30 14:21:00 +0200

    Modal login estyles

--- src/scripts/moduls/menus.js
+++ src/scripts/moduls/menus.js
@@ -11,17 +11,32 @@ hamburger.addEventListener("click", function () {
 });
 
 /* modal registro */
-const btnRegistro = document.getElementById("linkLogin");
+const btnRegistro = document.getElementById("linkRegistrarse");
 const modalRegistro = document.getElementById("modalRegistro");
 const registroBtnClose = document.getElementById("registroBtnClose");
 
 btnRegistro.addEventListener("click", function () {
     modalRegistro.classList.toggle("is-active");
-
     hamburger.classList.toggle("is-active");
     header.classList.toggle("is-active");
 });
 
 registroBtnClose.addEventListener("click", function () {
-    modalRegistro.classList.toggle("is-active")
+    modalRegistro.classList.toggle("is-active");
 });
+
+/* Modal Login */
+const btnLogin = document.getElementById("linkIdentificarse");
+const modalLogin = document.getElementById("modalLogin");
+const loginBtnClose = document.getElementById("loginBtnClose");
+
+btnLogin.addEventListener("click", function () {
+    modalLogin.classList.toggle("is-active");
+    hamburger.classList.toggle("is-active");
+    header.classList.toggle("is-active");
+});
+
+loginBtnClose.addEventListener("click", function () {
+    modalLogin.classList.toggle("is-active");
+});
+
