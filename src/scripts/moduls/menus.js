c2c858b453b72a9ae13c0937925957ab509a5bf2
Dario MF <ferrada_86@hotmail.com>
2020-07-30 18:57:38 +0200

    RWD menu y modales hasta 1080px

--- src/scripts/moduls/menus.js
+++ src/scripts/moduls/menus.js
@@ -6,9 +6,10 @@ const header = document.querySelector(".header");
 hamburger.addEventListener("click", function () {
 
     hamburger.classList.toggle("is-active");
-
     header.classList.toggle("is-active");
 });
+
+
 
 /* modal registro */
 const btnRegistro = document.getElementById("linkRegistrarse");
@@ -16,7 +17,13 @@ const modalRegistro = document.getElementById("modalRegistro");
 const registroBtnClose = document.getElementById("registroBtnClose");
 
 btnRegistro.addEventListener("click", function () {
-    modalRegistro.classList.toggle("is-active");
+
+    if (document.querySelector("#modalLogin.is-active")) {
+        modalLogin.classList.toggle("is-active");
+        modalRegistro.classList.toggle("is-active");
+    } else {
+        modalRegistro.classList.toggle("is-active");
+    }
     hamburger.classList.toggle("is-active");
     header.classList.toggle("is-active");
 });
@@ -25,13 +32,21 @@ registroBtnClose.addEventListener("click", function () {
     modalRegistro.classList.toggle("is-active");
 });
 
+
+
 /* Modal Login */
 const btnLogin = document.getElementById("linkIdentificarse");
 const modalLogin = document.getElementById("modalLogin");
 const loginBtnClose = document.getElementById("loginBtnClose");
 
 btnLogin.addEventListener("click", function () {
-    modalLogin.classList.toggle("is-active");
+
+    if (document.querySelector("#modalRegistro.is-active")) {
+        modalRegistro.classList.toggle("is-active");
+        modalLogin.classList.toggle("is-active");
+    } else {
+        modalLogin.classList.toggle("is-active");
+    }
     hamburger.classList.toggle("is-active");
     header.classList.toggle("is-active");
 });
