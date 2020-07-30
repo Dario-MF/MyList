75523affd8e04e85fd0d0e2e6bc18d35d3827e0e
Dario-MF <44214019+Dario-MF@users.noreply.github.com>
2020-07-31 00:01:37 +0200

    update config.js

--- src/scripts/firebase/config.js
+++ src/scripts/firebase/config.js
@@ -1,14 +1,3 @@
 
 
-const config = {
-    apiKey: "AIzaSyCFVjxpKcUbbutN2kc8FYjrmqSLvxBXe-g",
-    authDomain: "to-do-list-84ef2.firebaseapp.com",
-    databaseURL: "https://to-do-list-84ef2.firebaseio.com",
-    projectId: "to-do-list-84ef2",
-    storageBucket: "to-do-list-84ef2.appspot.com",
-    messagingSenderId: "910841674524",
-    appId: "1:910841674524:web:3afecdcdea7160132918d9",
-    measurementId: "G-Q9X2VGK61H"
-};
 
-firebase.initializeApp(config);
