34e27f7cf5353c60897cf77cbdfe6bc5cb8e34d1
Dario MF <44214019+Dario-MF@users.noreply.github.com>
2020-08-07 09:42:55 +0200

    update

--- src/scripts/firebase/config.js
+++ src/scripts/firebase/config.js
@@ -1,13 +1,13 @@
 
 const config = {
-    apiKey: "AIzaSyCFVjxpKcUbbutN2kc8FYjrmqSLvxBXe-g",
-    authDomain: "to-do-list-84ef2.firebaseapp.com",
-    databaseURL: "https://to-do-list-84ef2.firebaseio.com",
-    projectId: "to-do-list-84ef2",
-    storageBucket: "to-do-list-84ef2.appspot.com",
-    messagingSenderId: "910841674524",
-    appId: "1:910841674524:web:3afecdcdea7160132918d9",
-    measurementId: "G-Q9X2VGK61H"
+    apiKey: "XXXXXXXXXXXXCCXCXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXXXXXXXX",
+    databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    measurementId: "XXXXXXXXXXXX"
 };
 
 firebase.initializeApp(config);
