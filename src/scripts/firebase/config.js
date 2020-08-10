dd0bf47470cefa07781931d1a0adfcddf0536cf1
Dario MF <ferrada_86@hotmail.com>
2020-08-10 19:59:02 +0200

    git ignore config

--- src/scripts/firebase/config.js
+++ src/scripts/firebase/config.js
@@ -1,13 +1,13 @@
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCFVjxpKcUbbutN2kc8FYjrmqSLvxBXe-g",
-    authDomain: "to-do-list-84ef2.firebaseapp.com",
-    databaseURL: "https://to-do-list-84ef2.firebaseio.com",
-    projectId: "to-do-list-84ef2",
-    storageBucket: "to-do-list-84ef2.appspot.com",
-    messagingSenderId: "910841674524",
-    appId: "1:910841674524:web:3afecdcdea7160132918d9",
-    measurementId: "G-Q9X2VGK61H"
+const firebaseConfig = {
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    measurementId: "XXXXXXXXXXXXXXX"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
